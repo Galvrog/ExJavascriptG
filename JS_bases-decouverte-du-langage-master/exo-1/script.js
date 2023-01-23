83361e4439de73087f39730c7b5cc18981161230
Grégoire GEFFRAY <113893563+Galvrog@users.noreply.github.com>
2023-01-23 12:31:47 +0100

    Decouverte

---
+++ JS_bases-decouverte-du-langage-master/exo-1/script.js
@@ -0,0 +1,15 @@
+// you can write js here
+var kelvins = prompt("Quelle est la température en Kelvins aujourd'hui ?");
+
+var kelvins = 294;
+var celsius = kelvins - 273;
+var fahrenheit = celsius * (9/5) + 32;
+
+console.log('Température en Kelvins')
+console.log(kelvins)
+console.log(Math.floor(kelvins));
+
+console.log('Température en fahrenheit')
+console.log(fahrenheit);
+console.log(Math.floor(fahrenheit));
+
